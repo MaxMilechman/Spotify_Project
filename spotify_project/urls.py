bd5c91ee5cf2f3fcfd16cdfb3d2f284ab43534f7
MaxMilechman <m.milechman@gmail.com>
2021-01-15 14:52:06 -0800

    refactor to change project name

--- spotify_project/urls.py
+++ spotify_project/urls.py
@@ -1,4 +1,4 @@
-"""spotify_project_new URL Configuration
+"""spotify_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
